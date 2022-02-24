fd6390c76d767bb4e25fce02d1774b7369319462
Karanbir Singh Janjua <karanbirjanjua@gmail.com>
2022-02-24 13:03:10 +1030

    Added modal and controller

---
+++ Login(MVC)/Login(MVC)/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Login_MVC_.Models;
+
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Verify(Account acc) 
+        {
+            return View(); 
+        }
+    }
+}
